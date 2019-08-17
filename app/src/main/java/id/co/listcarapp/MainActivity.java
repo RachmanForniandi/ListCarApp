5d947b5bfef3b830939524ba6d5ade9468c02ee4
RachmanForniandi <rachmanforniandi@gmail.com>
2019-08-17 11:59:06 +0700

    initial commit preparing layouts

---
+++ app/src/main/java/id/co/listcarapp/MainActivity.java
@@ -0,0 +1,44 @@
+package id.co.listcarapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.list_owner) RecyclerView listOwner;
+    @BindView(R.id.btn_car_info) Button btnCarInfo;
+    @BindView(R.id.btn_owner_info)Button btnOwnerInfo;
+    @BindView(R.id.img_car_logo) ImageView imgCarLogo;
+    @BindView(R.id.tv_model_car_name) TextView tvModelCarName;
+    @BindView(R.id.tv_name_owner)TextView tvNameOwner;
+    @BindView(R.id.tv_contact_no)TextView tvContactNo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        btnOwnerInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnCarInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
